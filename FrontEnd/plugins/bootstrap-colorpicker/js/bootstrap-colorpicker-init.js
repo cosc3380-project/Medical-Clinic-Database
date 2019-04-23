77fbe4b6774f56ee7e159a050334cb1106193ca7
Mustafa AL-Jaburi <maljaburi@gmail.com>
2019-04-22 21:58:50 -0500

    del

--- FrontEnd/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker-init.js
+++
@@ -1,3 +0,0 @@
-$(function() { $('#colorinput1').colorpicker({ color: "", format: "hex" }); }); 
-$(function() { $('#colorinput2').colorpicker({ color: "#AA3399", format: "rgba"}); }); 
-$(function() { $('#cp1').colorpicker(); });
